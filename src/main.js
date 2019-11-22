6303890b1d9f79c69afe8c92c04c88f82589514d
Dmitriy <a20150209@gmail.com>
2019-11-22 19:02:11 +0300

    remarks have been fixed

--- src/main.js
+++ src/main.js
@@ -11,16 +11,13 @@ const selectorElement = {
 
 const createMenuTemplate = () => {
   return (
-    `<section class="main__control control container">
-      <h1 class="control__title">TASKMANAGER</h1>
-      <section class="control__btn-wrap">
-        <input type="radio" name="control" id="control__new-task" class="control__input visually-hidden" />
-        <label for="control__new-task" class="control__label control__label--new-task">+ ADD NEW TASK</label>
-        <input type="radio" name="control" id="control__task" class="control__input visually-hidden" checked />
-        <label for="control__task" class="control__label">TASKS</label>
-        <input type="radio" name="control" id="control__statistic" class="control__input visually-hidden" />
-        <label for="control__statistic" class="control__label">STATISTICS</label>
-      </section>
+    `<section class="control__btn-wrap">
+      <input type="radio" name="control" id="control__new-task" class="control__input visually-hidden" />
+      <label for="control__new-task" class="control__label control__label--new-task">+ ADD NEW TASK</label>
+      <input type="radio" name="control" id="control__task" class="control__input visually-hidden" checked />
+      <label for="control__task" class="control__label">TASKS</label>
+      <input type="radio" name="control" id="control__statistic" class="control__input visually-hidden" />
+      <label for="control__statistic" class="control__label">STATISTICS</label>
     </section>`
   );
 };
@@ -337,4 +334,4 @@ renderTemplate(cardsWrapperElement, createEditCardTemplate());
 for (let i = 0; i < MAX_CARDS_QUANTITY; i++) {
   renderTemplate(cardsWrapperElement, createCardTemplate());
 }
-//test ssh
+
